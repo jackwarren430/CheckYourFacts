912de3d56e3bf8aa46ff7f1040efa04fc91edc11
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2023-07-06 17:43:34 -0400

    added working button

--- popup.js
+++ popup.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('h1').textContent = 'Hello, Chrome Extension!';
+  //document.querySelector('h1').textContent = 'Hello, Chrome Extension!';
 });
+
+document.getElementById('checkButton').addEventListener('click', factCheck);
+
+function factCheck() {
+    let fact = document.getElementById('statement').value;
+    alert(fact);
+}
+
